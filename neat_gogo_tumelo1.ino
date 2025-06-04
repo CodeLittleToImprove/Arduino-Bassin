cba258f8a366e8e6c514ce6eced6b56119339129
CodeLittleToImprove <55624670+CodeLittleToImprove@users.noreply.github.com>
2025-06-04 17:28:58 +0200

    Update neat_gogo_tumelo1.ino
    finished_ex00_no_bonus

--- neat_gogo_tumelo1.ino
+++ neat_gogo_tumelo1.ino
@@ -2,12 +2,14 @@ const int buttonPin = 2;
 const int ledOne = 13;
 const int ledTwo = 12;
 const int ledThree = 8;
+const unsigned long blinkInterval = 500;
 
 int		buttonState = HIGH; //default is 1
 int		lastButtonState = HIGH; //default is 1
 bool	ledStateGreen = true; // default turned on
 bool	blinking = false;
-
+bool currentBlinkLedState = LOW;
+unsigned long previousMillis = 0;
 
 void setup()
 {
@@ -15,34 +17,55 @@ void setup()
   pinMode(ledOne, OUTPUT);
   pinMode(ledTwo, OUTPUT);
   pinMode(ledThree, OUTPUT);
+
   Serial.begin(9600);
+
   digitalWrite(ledThree, ledStateGreen);
 }
 
+void toggleGreenLed()
+{
+  ledStateGreen = !ledStateGreen;
+  digitalWrite(ledThree, ledStateGreen);
+  Serial.print("Green LED is now ");
+  Serial.println(ledStateGreen ? "ON" : "OFF");
 
-void loop()
+  blinking = !ledStateGreen;
+}
+
+void handleButton()
 {
   buttonState = digitalRead(buttonPin);
   Serial.print("Button state: ");
   Serial.println(buttonState);
-  
+
   if (lastButtonState == HIGH && buttonState == LOW) // detects change from 1 to 0
   {
-    ledStateGreen = !ledStateGreen;
-    digitalWrite(ledThree, ledStateGreen);
-    Serial.print("Green LED is now ");
-    Serial.println(ledStateGreen ? "ON" : "OFF");
-    blinking = !ledStateGreen;
+    toggleGreenLed();
+    delay(200);
   }
-  
   lastButtonState = buttonState;
-  if (blinking)
+
+}
+void handle_BlinkingLeds()
+{
+  if (!blinking)
   {
-    digitalWrite(ledTwo, 1);
-    digitalWrite(ledOne, 1);
-    delay(500);
-    digitalWrite(ledTwo, 0);
-    digitalWrite(ledOne, 0);
-    delay(500);
+    digitalWrite(ledOne, LOW);
+    digitalWrite(ledTwo, LOW);
+    return;
   }
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= blinkInterval)
+    {
+      previousMillis = currentMillis;
+      currentBlinkLedState = !currentBlinkLedState;
+      digitalWrite(ledOne, currentBlinkLedState ? HIGH : LOW);
+      digitalWrite(ledTwo, currentBlinkLedState ? HIGH : LOW);
+    }
+}
+void loop()
+{
+  handleButton();
+  handle_BlinkingLeds();
 }
