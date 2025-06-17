b5a3e41f1494309cedc7ec10a0572213e5003c56
CodeLittleToImprove <55624670+CodeLittleToImprove@users.noreply.github.com>
2025-06-17 11:43:41 +0200

    Update ex01_bonus.ino
    small refactor

--- ex01_bonus.ino
+++ ex01_bonus.ino
@@ -8,89 +8,77 @@ const int d5 = 10;
 const int d6 = 11;
 const int d7 = 12;
 
-int brightness = 0;
-int pwmValue = 0;
+int startBrightness = 50;
+int brightness = startBrightness;
 String command;
 
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
-
 void setup()
 {
   Serial.begin(9600);
-  delay(250);
   pinMode(ledGreen, OUTPUT);
-
-  lcd.clear();
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-
-  // Serial.print("total:");
-  // Serial.println(total);
+  lcd.print("Wait for cmd...");
 }
 
 void controlLed(int step)
 {
   brightness += step;
+
   if (brightness < 1)
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("value invalid:"); 
     brightness = 1;
-  } 
-  if (brightness >= 101)
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("value invalid:"); 
+  else if (brightness > 100)
     brightness = 100;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  if (brightness == 1 && step < 0)
+  {
+    lcd.print("Min reached");
+  } 
+  else if (brightness == 100 && step > 0)
+  {
+    lcd.print("Max reached");
+  } 
+  else
+  {
+    lcd.print("Updated value");
   }
-  // lcd.clear();
-  // lcd.setCursor(0, 0);
-  // lcd.print("value not allowed:");
 }
 
 void loop()
 {
- if (Serial.available())
- {
-  command = Serial.readStringUntil('\n');
-  command.trim();
-  if (command.equalsIgnoreCase("brighter"))
+  if (Serial.available())
   {
-    lcd.clear();
-    controlLed(10);
-  }
-  else if (command.equalsIgnoreCase("darker"))
-  {
-    lcd.clear();
-    controlLed(-10);
-  }
-  else
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("invalid cmd:");
-  }
- }
+    command = Serial.readStringUntil('\n');
+    command.trim();
 
- pwmValue = map(brightness, 1, 100, 0, 255);
- analogWrite(ledGreen, pwmValue);
+    if (command.equalsIgnoreCase("brighter"))
+    {
+      controlLed(10);
+    } 
+    else if (command.equalsIgnoreCase("darker"))
+    {
+      controlLed(-10);
+    } 
+    else
+    {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Invalid command");
+    }
+  }
 
- lcd.setCursor(0, 1);
- lcd.print("Brightness:");
- if (brightness == 0)
- {
-    lcd.print(0);
-    brightness++;
-    return;
- }
- else
- {
-    lcd.print(brightness);
-    lcd.print("%   ");
- }
+  int pwmValue = map(brightness, 1, 100, 0, 255);
+  analogWrite(ledGreen, pwmValue);
 
- delay(250);
+  lcd.setCursor(0, 1);
+  lcd.print("Brightness: ");
+  lcd.print(brightness);
+  lcd.print("%   ");
+
+  delay(250);
 }
